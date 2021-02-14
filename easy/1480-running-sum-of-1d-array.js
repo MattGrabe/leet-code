8271cd3a6daf53decfeffcdf0328c18dc84a8710
Matt Grabe <77252186+MattGrabe@users.noreply.github.com>
2021-02-14 14:14:58 -0800

    Remove comments from 1480-running-sum-of-1d-array

--- easy/1480-running-sum-of-1d-array.js
+++ easy/1480-running-sum-of-1d-array.js
@@ -1,12 +1,12 @@
-// const nums = [1, 2, 3, 4];
+const nums = [1, 2, 3, 4];
 
-// var runningSum = function (nums) {
-//   const stack = [];
-//   for (let i = 0; i < nums.length; i++) {
-//     if (stack.length === 0) stack.push(nums[i]);
-//     else stack.push(stack[stack.length - 1] + nums[i]);
-//   }
-//   return stack;
-// };
+var runningSum = function (nums) {
+  const stack = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (stack.length === 0) stack.push(nums[i]);
+    else stack.push(stack[stack.length - 1] + nums[i]);
+  }
+  return stack;
+};
 
-// console.log(runningSum(nums));
+console.log(runningSum(nums));
